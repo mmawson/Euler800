3a875473a428eb999d44addd79e1f180e6fc1725
Matt <matt.mawson@gmail.com>
2023-12-11 18:30:47 -0500

    4 Processes on backwards summation

--- ProjectE800v1.py
+++ ProjectE800v1.py
@@ -54,34 +54,28 @@ Created on Thu Dec  7 11:07:31 2023
 import csv
 import numpy as np
 import time
-from multiprocessing import Process, Lock
+from multiprocessing import Process
 
+upper = 800800 ** 800800
 
-totalCount = 0
-upper = 800 ** 800
-
-def solveC(l, i, data_array1, data_array2):
+def solveC(i, data_array1, data_array2):
     
-    table = open("800800Test.csv", "a")
-    iCount = 1;
+    table = open("800800Grail"+str(i)+".csv", "a")
 
     for i in range(0, len(data_array1)):
-        if iCount == 0:
-            break
-        iCount = 0
         data_array2.pop(0)
-        for j in range(0, len(data_array2)):
-            if ((pow(data_array2[j],data_array1[i])*pow(data_array1[i],data_array2[j])) <= upper):
-                iCount += 1
+        for j in range(len(data_array2)-1,-1,-1):
+            if ((pow(data_array2[j],data_array1[i])*pow(data_array1[i],data_array2[j])) > upper):
+                continue
                 #totalCount += 1
-            elif ((pow(data_array2[j],data_array1[i])*pow(data_array1[i],data_array2[j])) > upper):
+            elif ((pow(data_array2[j],data_array1[i])*pow(data_array1[i],data_array2[j])) <= upper):
+                table.writelines("{0}:{1}\n".format(data_array1[i],j+1))
+                data_array2.pop(0)
+                data_array2.pop(0)
+                data_array2.pop(0)
                 break
-        table.writelines("{0}:{1}\n".format(data_array1[i],iCount))
-
-    #table.writelines("Total: {0}".format(totalCount)) 
-    #print("")
     table.close()
-    return iCount
+    return
 
 if __name__ == '__main__':
     
@@ -89,79 +83,25 @@ if __name__ == '__main__':
     
     """ Split up the array of primes here """
     
-    with open('/Users/mattmawson/Desktop/Euler800/Primes-beta.csv', 'r') as f: 
+#    with open('/Users/mattmawson/Desktop/Euler800/Primes-beta.csv', 'r') as f: 
+    with open('/Users/mattmawson/Desktop/Euler800/PrimesUltra.csv', 'r') as f: 
         reader = csv.reader(f)
         data = list(reader)
         data = [eval(i) for i in data[0]]
-    #    data_array = np.array(data, dtype=object)
         data_array1 = data.copy()
         data_array2 = data.copy()
     
-    print(data_array1[6])
-    
-    
-    
-    lock = Lock()
-    p = Process(target=solveC, args=(lock, 0, data_array1, data_array2,))
-    #q = Process(target=solveC, args=(lock, 1, [3], [5,7,11],))
+    p = Process(target=solveC, args=(1, data_array1[0::4], data_array2,))
+    q = Process(target=solveC, args=(2, data_array1[1::4], data_array2[1::],))
+    r = Process(target=solveC, args=(3, data_array1[2::4], data_array2[2::],))
+    s = Process(target=solveC, args=(4, data_array1[3::4], data_array2[3::],))
     p.start()
-    #q.start()
-    print(p.join())
-    #q.join()
-
-    #print(data)
+    q.start()
+    r.start()
+    s.start()
+    p.join()
+    q.join()
+    r.join()
+    s.join()
 
     print("--- %s seconds ---" % (time.time() - start_time))
-
-"""
-
-totalCount = 0
-iCount = 1
-upper = 800800 ** 800800
-
-
-start_time = time.time()
-
-with open('/Users/mattmawson/Desktop/Euler800/PrimesUltra.csv', 'r') as f: 
-    reader = csv.reader(f)
-    data = list(reader)
-    data = [eval(i) for i in data[0]]
-#    data_array = np.array(data, dtype=object)
-    data_array1 = data.copy()
-    data_array2 = data.copy()
-
-table = open("800800Result.csv", "a")
-
-for i in range(0, len(data_array1)):
-    if iCount == 0:
-        break
-    iCount = 0
-    data_array2.pop(0)
-    for j in range(0, len(data_array2)):
-        if ((pow(data_array2[j],data_array1[i])*pow(data_array1[i],data_array2[j])) <= upper):
-            iCount += 1
-            totalCount += 1
-        elif ((pow(data_array2[j],data_array1[i])*pow(data_array1[i],data_array2[j])) > upper):
-            break
-    table.writelines("{0}:{1}\n".format(data_array1[i],iCount))
-
-table.writelines("Total: {0}".format(totalCount)) 
-print(totalCount)
-print("--- %s seconds ---" % (time.time() - start_time))
-
-table.close()
-
-#print(upper)
-#    csv_reader = csv.reader(csv_file, delimiter=',')
-#    line_count = 0
-#    for row in csv_reader:
-#        print(row[1])
-#        print("Next number")
-#        if line_count == 0:
-#            print(f'Column names are {", ".join(row)}')
-#            line_count += 1
-#        else:
-#            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-#            line_count += 1
-#    print(f'Processed {line_count} lines.')
-"""
